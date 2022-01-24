c6bfd3749e127334bbe57e5bc11e6b60fc01bab5
Angel Castañeda <74835918+herbstluft@users.noreply.github.com>
2022-01-23 23:52:59 -0600

    Update install_sound.sh

--- install_sound.sh
+++ install_sound.sh
@@ -1,3 +1,1 @@
-sudo pacman -S alsa-utils alsa-plugins alsa-oss
-sudo modprobe snd_seq_oss snd_pcm_oss snd_mixer_oss
-speaker-test -c 2
+sudo pacman -S --noconfirm gstreamer gst-plugins-bad gst-plugins-base  gst-plugins-base-libs gst-plugins-good gst-plugins-ugly xine-lib libdvdcss libdvdread dvd+rw-tools vlc lame
